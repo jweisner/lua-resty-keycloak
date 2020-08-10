888011966ae32713e1319ee65cc7eaba1e644b91
Jesse Weisner <jesse@weisner.ca>
2020-08-10 12:51:47 -0700

    assert keycloak_config return data type

--- lib/resty/keycloak.lua
+++ lib/resty/keycloak.lua
@@ -122,6 +122,8 @@ end
 -- returns the base URL for the configured Keycloak realm
 local function keycloak_realm_url()
     local config          = keycloak_config()
+    assert(type(config) == "table")
+
     local auth_server_url = config["auth-server-url"]
 
     -- make sure the auth server url ends in /
