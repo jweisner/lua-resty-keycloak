f9c8d77800ebf9e577fa99e18968df4eaeaef275
Jesse Weisner <jesse@weisner.ca>
2020-10-16 18:55:50 -0700

    Implemented wildcard path match
    #1
    
    Testing required

--- lib/resty/keycloak.lua
+++ lib/resty/keycloak.lua
@@ -456,23 +456,42 @@ local function keycloak_uri_path_match(subject, test)
     local subject = subject or ""
     local test    = test    or ""
 
+    -- check for simple exact match
     if subject == test then
         return string.len(test)
     end
 
-    if test == '/' then
+    -- if the pattern has no wildcards, stop here
+    if string.find(test, "[?*]") == nil then
+        return nil
+    end
+
+    -- shortcut "whole site" glob
+    if test == '/*' then
         return 1
     end
 
-    local s,e = string.find(subject,test)
-    local testlen = string.len(test)
-    if (s == 1) and (e == testlen) and (string.sub(test,-1) == '/') then
-        -- match depth is the whole policy uri and last character of policy uri is slash
-        -- matches directory in policy to a directory path of the request path
-        return e
-    end
+    -- wildcard match depth is the number of characters matched without wildcard expansion
+    local test_depth = string.len(string.gsub(test,"[?*]",""))
 
-    return nil
+    -- Convert globs to lua patterns
+    -- ... escape all .
+    test = string.gsub(test, "%.", "\\.")
+    -- ... replace all ? with .
+    test = string.gsub(test, "%?", '.')
+    -- ... replace all * with .+
+    test = string.gsub(test, "%*", ".+")
+
+    -- get the beginning and end positions in the subject string that match the test
+    local start_match, end_match = string.find(subject, test)
+    -- it's only a match if it's the whole string (1 to length)
+    if start_match == 1 and end_match == string.len(subject) then
+        -- match successful, return the number of non-wildcard characters in the test as a "match quality rating"
+        return test_depth
+    else
+        -- match failed
+        return nil
+    end
 end
 
 local function keycloak_scopes_to_lookup_table(scope_hash)
