5a7e58bd5a1b8274b0f3efd54fa555a1bf1b7f38
Jesse Weisner <jesse@weisner.ca>
2021-04-26 16:36:49 -0700

    fix "property of nil" errors

--- lib/resty/keycloak.lua
+++ lib/resty/keycloak.lua
@@ -321,7 +321,7 @@ local function keycloak_export_attributes(token_attributes)
     --             "Current Employee"
     --         ]
     --     },
-    if type(token_attributes.realm_access.roles) == "table" then
+    if type(token_attributes.realm_access) == "table" and type(token_attributes.realm_access.roles) == "table" then
         ngx.var.oid_realm_roles = table.concat(token_attributes.realm_access.roles, ",")
     else
         ngx.var.oid_realm_roles = ""
@@ -337,7 +337,7 @@ local function keycloak_export_attributes(token_attributes)
     --             ]
     --         }
     --     },
-    if type(token_attributes.resource_access.account.roles) == "table" then
+    if type(token_attributes.resource_access) == "table" and type(token_attributes.resource_access.account.roles) == "table" then
         ngx.var.oid_resource_roles = table.concat(token_attributes.resource_access.account.roles, ",")
     else
         ngx.var.oid_resource_roles = ""
